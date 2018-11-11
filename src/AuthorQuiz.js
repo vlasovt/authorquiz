ce429e4be229bb7c64a2d40a3c6ea3b331c46211
vlasovt <tarasvlasov@gmail.com>
2018-11-11 13:10:08 -0500

    added data

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './AuthorQuiz.css';
 import './bootstrap.min.css';
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div className="container-fluid">Author Quiz</div>
-    );
-  }
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Turn({author, books}) {
+  return (
+    <div className="row turn" style={{backgroundColor: "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} alt="" className="authorImage" alt="author"/>
+      </div>
+      <div className="col-6">
+        {books.map((title) => <Book title={title} key={title} /> )}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return (
+    <div className="row">
+    </div>
+  );
+}
+
+function Book({title}) {
+  return (
+    <div className="answer">
+    <h4>{title}</h4>
+    </div>
+  );
+}
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,73 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.png',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining', 'IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesdickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/williamshakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+];
+
+function getTurnData(authors){
+    const allBooks = authors.reduce(function(p,c,i){
+        return p.concat(c.books);
+    }, []);
+
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find(
+            (author) => author.books.some(
+                (title) => title === answer
+            )
+        )
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors)
+}
+
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
